cde927626386a3dc45cd8b5a43144a59ba070ce5
Jasper <jave.ong4@yahoo.com>
2016-05-31 15:17:11 -0400

    Pick up bananas

--- exercise6.rb
+++ exercise6.rb
@@ -13,3 +13,9 @@ grocery_list << "rice"
 asterisk(grocery_list)
 
 puts grocery_list.count
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas"
+end
